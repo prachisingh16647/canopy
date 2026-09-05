acd71f704e10f9eea0986b8254dbc70e6eedb245
prachi singh <prachi.singh16647@gmail.com>
2026-09-05 17:49:24 +0530

    Update reports.js

--- static/reports.js
+++ static/reports.js
@@ -1,23 +1,13 @@
 fetch("/library/api/reports/")
   .then(res => res.json())
   .then(data => {
+    console.log("REPORT DATA:", data);
 
-    document.getElementById("rTotalBooks").innerText =
-      data.total_books;
-
-    document.getElementById("rTotalMembers").innerText =
-      data.total_members;
-
-    document.getElementById("rBorrowed").innerText =
-      data.active_borrowed;
-
-    document.getElementById("rOverdue").innerText =
-      data.overdue_books;
-
-    document.getElementById("rReturned").innerText =
-      data.returned_books;
-
+    document.getElementById("rTotalBooks").innerText = data.total_books;
+    document.getElementById("rTotalMembers").innerText = data.total_members;
+    document.getElementById("rBorrowed").innerText = data.active_borrowed;
+    document.getElementById("rOverdue").innerText = data.overdue_books;
+    document.getElementById("rReturned").innerText = data.returned_books;
   })
-  .catch(error => {
-    console.error("Error loading reports:", error);
-  });
+  .catch(error => console.error("Error loading reports:", error));
+
